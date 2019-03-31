53ebd92c549b74c016a852480a07dda061a5894e
Apache <apache@fedora-home.localdomain>
2019-03-31 07:51:04 +0200

    commit

--- Test/Unit/Console/Command/CleannerCommandTest.php
+++ Test/Unit/Console/Command/CleannerCommandTest.php
@@ -8,43 +8,38 @@ namespace Angeldm\Cleanner\Test\Unit\Console\Command;
 use Symfony\Component\Console\Tester\CommandTester;
 use Angeldm\Cleanner\Console\Command\CleannerCommand;
 
-class CleannerCommandTest extends \PHPUnit_Framework_TestCase
+class CleannerCommandTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @var \Magento\Framework\App\Cache|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $cache;
+    /**
+     * @var \Magento\Framework\App\State\CleanupFiles|\PHPUnit_Framework_MockObject_MockObject
+     */
+
+    private $cleanupFiles;
     /**
      * @var GreetingCommand
      */
     private $command;
     public function setUp()
     {
-        $this->command = new CleannerCommand();
+        $this->cache = $this->createMock(\Magento\Framework\App\Cache::class);
+        $this->cleanupFiles = $this->createMock(\Magento\Framework\App\State\CleanupFiles::class);
+	$this->command = new CleannerCommand(
+            $this->cache,
+            $this->cleanupFiles
+	);
     }
-    public function testExecuteAnonymous()
-    {
-        $commandTester = new CommandTester($this->command);
-        $commandTester->execute(
-            [
-                '-a' => true
-            ]
-        );
-        $this->assertContains('Hello Anonymous!', $commandTester->getDisplay());
-    }
+    
     public function testExecuteName()
     {
         $commandTester = new CommandTester($this->command);
-        $commandTester->execute(
-            [
-                GreetingCommand::NAME_ARGUMENT => 'Test'
-            ]
-        );
-        $this->assertContains('Hello Test!', $commandTester->getDisplay());
-    }
-    /**
-     * @expectedException \InvalidArgumentException
-     * @expectedExceptionMessage Argument name is missing
-     */
-    public function testExecuteError()
-    {
-        $commandTester = new CommandTester($this->command);
         $commandTester->execute([]);
+	$this->assertContains('Cache cleared successfully.', $commandTester->getDisplay());
+	$this->assertContains('Generated classes cleared successfully.', $commandTester->getDisplay());
+	$this->assertContains('Generated static view files cleared successfully.', $commandTester->getDisplay());
     }
+
 }
