2f15340ab957bd4bf141b3ca80afc7cfb754318e
àngel <angeldm@gmail.com>
2019-03-31 04:20:56 +0200

    Update CleannerCommand.php

--- Console/Command/CleannerCommand.php
+++ Console/Command/CleannerCommand.php
@@ -1,31 +1,22 @@
 <?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
+
 namespace Angeldm\Cleanner\Console\Command;
 
-use Magento\Framework\App\Cache;
-use Magento\Framework\App\State\CleanupFiles;
-use Magento\Framework\ObjectManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CleannerCommand extends Command
 {
-    private $cache;
-    private $cleanupFiles;
-    private $objectManager;
+    protected $cache;
+    protected $cleanupFiles;
 
     public function __construct(
-        Cache $cache,
-        CleanupFiles $cleanupFiles,
-        ObjectManagerInterface $objectManager
-      ) {
+        \Magento\Framework\App\Cache $cache,
+        \Magento\Framework\App\State\CleanupFiles $cleanupFiles
+    ) {
         $this->cache = $cache;
         $this->cleanupFiles = $cleanupFiles;
-        $this->objectManager = $objectManager;
         parent::__construct();
     }
     /**
@@ -36,20 +27,18 @@ class CleannerCommand extends Command
         OutputInterface $output
     ) {
         $output->writeln("Hello ");
-        $this->cleanup($input, $output);
+        $this->cleanup($output);
     }
 
-    protected function cleanup(InputInterface $input, OutputInterface $output)
+    protected function cleanup(OutputInterface $output)
     {
         $this->cache->clean();
         $output->writeln('<info>Cache cleared successfully.</info>');
         $this->cleanupFiles->clearCodeGeneratedClasses();
-        $mode = $this->objectManager->create(\Magento\Deploy\Model\Mode::class, ['input' => $input,'output' => $output]);
-        if ($mode->getMode() == \Magento\Framework\App\State::MODE_PRODUCTION) {
-            $output->writeln("<info>Generated classes cleared successfully. Please run the 'setup:di:compile' command to generate classes.</info>");
-        } else {
-            $output->writeln("<info>Generated classes cleared successfully.</info>");
-        }
+            $output->writeln(
+                "<info>Generated classes cleared successfully. "
+                ."Please run the 'setup:di:compile' command to generate classes.</info>"
+            );
 
         $this->cleanupFiles->clearMaterializedViewFiles();
         $output->writeln('<info>Generated static view files cleared successfully.</info>');
@@ -63,16 +52,5 @@ class CleannerCommand extends Command
         $this->setName("cache:clean:static");
         $this->setDescription("Clean");
         parent::configure();
-    }
-
-    public function getModeObject()
-    {
-        return $this->objectManager->create(
-            \Magento\Deploy\Model\Mode::class,
-            [
-            'input' => new \Symfony\Component\Console\Input\ArrayInput([]),
-            'output' => $this->output,
-        ]
-    );
     }
 }
